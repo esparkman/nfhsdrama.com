25eccecd86b8587891bdb157d13db0894af70744
Evan Sparkman <me@evansparkman.com>
2019-06-19 23:23:35 -0400

    Fix typo in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,8 +15,8 @@ module.exports = {
     {
       resolve: "gatsby-source-contentful",
       options: {
-        spaceId
-        accessToken
+        spaceId: spaceId,
+        accessToken: accessToken
       },
     },
   ],
