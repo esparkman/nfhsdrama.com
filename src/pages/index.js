e36a9c142a92b1ff8f05ad8e4b3647881254a6f2
Evan Sparkman <me@evansparkman.com>
2019-08-25 22:00:31 -0400

    Add 404 page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import Gallery from "../components/Gallery"
+import Gallery from "../components/gallery"
 
 export default () => (
   <Layout>
