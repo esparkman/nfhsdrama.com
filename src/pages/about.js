6066b5cb7f53189878bec2c476a50c0f6650c725
Evan Sparkman <me@evansparkman.com>
2019-11-13 20:04:36 -0500

    Add Drama Team

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,56 @@
 import React from "react"
-import Layout from '../components/layout'
+import Layout from "../components/layout"
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
 
-export default () => (
-  <Layout>
-    <div className="mx-4">
-      <h1 className="text-3xl">About NFHS Theater</h1>
-      <p>Information about the Theatere Troupe</p>
-      <p>
-        I'll be adding the Drama Teams Profiles here later this evening. Outside of that, I need
-        content for this page. History of the Program, Accolades, fun facts. Anything.
-      </p>
-    </div>
-  </Layout>
-)
+export const About = props => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulDirector(sort: { fields: [name], order: ASC }) {
+        edges {
+          node {
+            id
+            slug
+            name
+            title
+            profilePicture {
+              fluid(maxWidth: 200, maxHeight: 250, quality: 80) {
+                ...GatsbyContentfulFluid_withWebp
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Layout>
+      <div className="mx-4">
+        <h1 className="text-3xl">About NFHS Theater</h1>
+        <p>Information about the Theatere Troupe</p>
+        <p>
+          I'll be adding the Drama Teams Profiles here later this evening.
+          Outside of that, I need content for this page. History of the Program,
+          Accolades, fun facts. Anything.
+        </p>
+
+        <div id="drama-team" className="flex flex-wrap">
+          {data.allContentfulDirector.edges.map(edge => {
+            return (
+              <div key={edge.node.id} className="w-auto mx-6 my-6">
+                <Img
+                  fluid={edge.node.profilePicture.fluid}
+                  alt={`${edge.name} - ${edge.title}`}
+                />
+                <p>{edge.node.name}</p>
+                <p>{edge.node.title}</p>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default About
--- src/pages/box_office.js
+++ src/pages/box_office.js
@@ -1,14 +1,14 @@
 import React from "react"
-import { Link, graphql, useStaticQuery } from "gatsby"
-import Img from 'gatsby-image'
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
 import Layout from "../components/layout"
 
 const BoxOffice = props => {
   const data = useStaticQuery(graphql`
     query {
-      allContentfulShow (
-        sort: { fields: showTime },
-        filter: { status: { eq: true } } 
+      allContentfulShow(
+        sort: { fields: showTime }
+        filter: { status: { eq: true } }
       ) {
         edges {
           node {
@@ -36,43 +36,58 @@ const BoxOffice = props => {
     <Layout>
       <div className="mx-4">
         <h1 className="text-3xl">Box Office</h1>
-        <h2 className="text-3xl font-extrabold">I need your guys with filler Content here.</h2>
+        <h2 className="text-3xl font-extrabold">
+          I need your guys with filler Content here.
+        </h2>
         <p>
-          These are the remaining shows for the Season. We need some fun helpful information to go
-          along with these guys.
+          These are the remaining shows for the Season. We need some fun helpful
+          information to go along with these guys.
         </p>
-      
+
         {data.allContentfulShow.edges.map(edge => {
           return (
             <div
               className="md:flex bg-white border rounded-lg my-4"
               key={edge.node.id}
             >
-              { edge.node.boxOfficeArt ?
+              {edge.node.boxOfficeArt ? (
                 <div className="relative pb-2/3">
-                  <Img 
+                  <Img
                     className="absolute object-cover rounded-l mr-4"
                     fixed={edge.node.boxOfficeArt.fixed}
                     alt="Box Office Art"
-                  /> 
-                </div> :
-                ''
-              }
+                  />
+                </div>
+              ) : (
+                ""
+              )}
               <div className="text-center md:text-left mx-6 my-4">
                 <h2 className="text-lg pb-2">{edge.node.title}</h2>
 
                 <div className="text-purple-500 text-sm">
-                  {edge.node.description ? edge.node.description.description : '' }
+                  {edge.node.description
+                    ? edge.node.description.description
+                    : ""}
                 </div>
                 <div className="mt-4 flex justify-between">
-                  { edge.node.showTime.map(show => {
+                  {edge.node.showTime.map(show => {
                     return (
-                    <div className="flex flex-col pt-1 uppercase font-semibold text-purple-500 text-xs" key={show.eventTime}>
-                      <span>{show.eventTime}</span>
-                      <a href={show.ticketUrl} target="_blank" rel="noopener noreferrer" className="rounded-lg px-1 py-1 bg-nfhspurple text-white text-center text-xs mt-2 mx-2">
-                      { show.ticketUrl ? 'Buy Tickets' : 'Tickets Unavailable' }
-                      </a>
-                    </div>
+                      <div
+                        className="flex flex-col pt-1 uppercase font-semibold text-purple-500 text-xs"
+                        key={show.eventTime}
+                      >
+                        <span>{show.eventTime}</span>
+                        <a
+                          href={show.ticketUrl}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="rounded-lg px-1 py-1 bg-nfhspurple text-white text-center text-xs mt-2 mx-2"
+                        >
+                          {show.ticketUrl
+                            ? "Buy Tickets"
+                            : "Tickets Unavailable"}
+                        </a>
+                      </div>
                     )
                   })}
                 </div>
